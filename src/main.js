ed077298a6d80957b4640dca0d33292079668d96
Pete Johanson <peter@peterjohanson.com>
2017-05-09 12:04:07 -0400

    Minor clean up.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import './style.scss';
 import 'material-design-lite/material.js';
 import 'dialog-polyfill/dialog-polyfill.css';
 import 'mdl-selectfield/src/selectfield/selectfield';
-// import 'mdl-selectfield/src/selectfield/selectfield.scss';
-// import 'mdl-selectfield/src/globals.scss';
 import Vue from 'vue';
 import App from './App';
 
